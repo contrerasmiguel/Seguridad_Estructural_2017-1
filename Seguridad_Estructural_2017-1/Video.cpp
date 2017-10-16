c141c13ec1af31c3e6a2520d4135f7b346fba894
Miguel Contreras <migueldevelop@gmail.com>
2017-10-15 21:05:31 -0400

    Hilo trabajador para que el video se grabe en background. Ahora se notifica la finalización de la grabación del video.

--- Seguridad_Estructural_2017-1/Video.cpp
+++ Seguridad_Estructural_2017-1/Video.cpp
@@ -3,39 +3,63 @@
 const string Video::FILE_EXTENSION = "avi";
 const int Video::RECORD_TIME = 7;
 
+Video::Video() : backgroundThread(NULL) { }
+
+Video::~Video()
+{
+	if (backgroundThread != NULL) {
+		backgroundThread->join();
+		delete backgroundThread;
+	}
+}
+
 string Video::generateFileName(DateTime* dt) const
 {
 	return "seguridad_" + to_string(dt->getYear()) + "-" + to_string(dt->getMonth()) + "-" + to_string(dt->getDay())
 		+ "_" + to_string(dt->getHour()) + "-" + to_string(dt->getMinute()) + "-" + to_string(dt->getSecond()) + "." + FILE_EXTENSION;
 }
 
-string Video::record(DateTime* dt) const
+string Video::record(DateTime* dt)
 {
 	string videoName = generateFileName(dt);
 
-	cout << "Video: camara grabando en archivo '" << videoName << "'." << endl;
-
-	/*VideoCapture vcap(0);
-	if (!vcap.isOpened()) {
-		cout << "Error opening video stream or file" << endl;
-		exit(1);
+	if (backgroundThread != NULL) {
+		backgroundThread->join();
+		delete backgroundThread;
 	}
 	
-	int video_lenght = 10;
-	long int begin = time(NULL);
-	long int end = begin + video_lenght;
-	
-	int frame_width = vcap.get(CV_CAP_PROP_FRAME_WIDTH);
-	int frame_height = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
-	
-	VideoWriter video(videoName, CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width, frame_height), true);
-	
-	for (;time(NULL) < end;) {
-		Mat frame;
-		vcap >> frame;
-		video.write(frame);
-		if (waitKey(30) >= 0) break;
-	}*/
+	backgroundThread = new thread(&Video::backgroundRecord, this, videoName);
 
 	return videoName;
+}
+
+void Video::backgroundRecord(string videoName) const 
+{
+	/*VideoCapture vcap(0);
+	if (!vcap.isOpened()) {
+	cout << "Error opening video stream or file" << endl;
+	exit(1);
+	}*/
+
+	int video_lenght = RECORD_TIME;
+	long int begin = time(NULL);
+	long int end = begin + video_lenght;
+
+	/*int frame_width = vcap.get(CV_CAP_PROP_FRAME_WIDTH);
+	int frame_height = vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+	VideoWriter video(videoName, CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width, frame_height), true);*/
+
+	cout << "Video: camara grabando en archivo '" << videoName << "'." << endl;
+	cout << "Video: se reactivara la deteccion de presencia despues de " << video_lenght << " segundos." << endl;
+
+	for (;time(NULL) < end;) {
+	/*Mat frame;
+	vcap >> frame;
+	video.write(frame);
+	if (waitKey(30) >= 0) break;*/
+	}
+
+	cout << "Video: la camara ha terminado de grabar." << endl;
+	cout << "Video: se ha reactivado la deteccion de presencia." << endl;
 }
--- Seguridad_Estructural_2017-1/Video.h
+++ Seguridad_Estructural_2017-1/Video.h
@@ -3,22 +3,30 @@
 #include "DateTime.h"
 #include <iostream>
 #include <string>
+#include <thread>
 
+using std::cerr;
 using std::cout;
 using std::endl;
 using std::string;
 using std::to_string;
+using std::thread;
 
 class Video
 {
 private:
 	const static string FILE_EXTENSION;
 	string generateFileName(DateTime* dt) const;
+	void backgroundRecord(string videoName) const;
+	thread* backgroundThread;
 
 public:
+	Video();
+	~Video();
+
 	// Tiempo de grabación en segundos.
 	const static int RECORD_TIME;
 
-	string record(DateTime* dt) const;
+	string record(DateTime* dt);
 };
 
